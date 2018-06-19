f67171660fe83d12c7b119d759ff03e21ed686ce
jtmg <jose.t.m.gouveia@gmail.com>
2018-06-19 23:21:00 +0100

    clean some files that were unnecessary

--- install.ps1
+++
@@ -1,13 +0,0 @@
-﻿# instalaÃ§Ã£o do chocolatey para ser mais fÃ¡cil instalar alguns softwares necessÃ¡rios
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-chocolatey feature enable -n allowGlobalConfirmation
-
-choco install vagrant
-choco install virtualbox
-choco install git.install
-new-item -path alias:git -value 'C:\Program Files\Git\bin\git.exe'
-New-Item -Path alias:vagrant -value 'C:\HashiCorp\Vagrant\bin\vagrant.exe'
-
-cd ~\Documents
-git clone https://github.com/jtmg/WindowsBox
-cd ~\Documents\WindowsBox\vagrant up
